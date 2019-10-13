4ac82df3f2bbcdf78eca75318f6fdb5173c2a866
pnhung177 <pnhung177@acegik.net>
2019-10-13 20:49:07 +0700

    Load the connection parameters from both system properties and the resource file

--- src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -19,6 +19,8 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
 
     public static String SLF4J_AMQP_CONFIG_FILE_KEY = "opflow.slf4j.amqp.configfile";
     
+    public static String SLF4J_AMQP_PARAM_PREFIX_KEY = "opflow.slf4j.amqp.param.";
+    
     /**
      * Declare the version of the SLF4J API this implementation is compiled against. The value of
      * this field is usually modified with each release. Per SLF4J,
@@ -51,22 +53,19 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
 
         Properties props = loadResourceFile();
         
-        String username = props.getProperty("username");
+        String username = getParameter(props, "username", String.class, null);
             
-        String password = props.getProperty("password");
+        String password = getParameter(props, "password", String.class, null);
 
-        String host = props.getProperty("host");
+        String host = getParameter(props, "host", String.class, "localhost");
 
-        Integer port = 5672;
-        if (props.getProperty("port") != null) {
-            port = Integer.valueOf(props.getProperty("port"));
-        }
+        Integer port = getParameter(props, "port", Integer.class, 5672);
 
-        String virtualHost = props.getProperty("virtualHost");
+        String virtualHost = getParameter(props, "virtualHost", String.class, null);
 
-        String exchange = props.getProperty("exchange");
+        String exchange = getParameter(props, "exchange", String.class, null);
 
-        String routingKey = props.getProperty("routingKey");
+        String routingKey = getParameter(props, "routingKey", String.class, null);
 
         RabbitMQConfig config = new RabbitMQConfig(username, password, host, port, virtualHost, exchange, routingKey);
 
@@ -93,6 +92,36 @@ public class StaticLoggerBinder implements LoggerFactoryBinder {
         } catch (IOException throwable) {
             throw new RuntimeException(throwable);
         }
+    }
+    
+    private String getParameterString(Properties props, String paramName) {
+        return EnvTool.getSystemProperty(SLF4J_AMQP_PARAM_PREFIX_KEY + paramName, props.getProperty(paramName));
+    }
+    
+    private <T> T getParameter(Properties props, String paramName, Class<T> type, T defval) {
+        String str = getParameterString(props, paramName);
+        if (str == null) {
+            return defval;
+        }
+        if (type == String.class) {
+            return (T) str;
+        }
+        if (type == Integer.class) {
+            return (T) Integer.valueOf(str);
+        }
+        if (type == Long.class) {
+            return (T) Long.valueOf(str);
+        }
+        if (type == Float.class) {
+            return (T) Float.valueOf(str);
+        }
+        if (type == Double.class) {
+            return (T) Double.valueOf(str);
+        }
+        if (type == Boolean.class) {
+            return (T) Boolean.valueOf(str);
+        }
+        return null;
     }
 
     @Override
